b5555c56f850f17febc33d0f8b174ca9468c9e71
Oleksii Drenov <leshadrenev@gmail.com>
2025-09-24 08:57:57 +0300

    fix critical issues

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM python:3.11.6-alpine3.18
 LABEL maintainer="leshadrenev@gmail.com"
 
-ENV PYTHOUNNBUFFERED 1
+ENV PYTHONUNBUFFERED 1
 
 WORKDIR app/
 
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
-COPY . .
+COPY app/ .
 
-CMD ["python", "app/main.py"]
+CMD ["python", "main.py"]
--- app/main.py
+++ app/main.py
@@ -5,19 +5,19 @@ import requests
 
 WEATHER_API_KEY = os.getenv("API_KEY")
 WEATHER_API_URL = "http://api.weatherapi.com/v1/current.json"
-
+CITY = "Paris"
 
 def get_weather() -> None:
 
     params = {
         "key": WEATHER_API_KEY,
-        "q": "Paris",
+        "q": CITY,
     }
 
     response = requests.get(WEATHER_API_URL, params=params)
     data = response.json()
     if response.status_code == 200:
-        print("Performing request to Weather API for city Paris ...")
+        print(f"Performing request to Weather API for city {CITY} ...")
         print(f"{data['location']['name']}/{data['location']['country']} "
               f"{data['location']['localtime']} "
               f"Weather: {data['current']['temp_c']} Celsius, "
